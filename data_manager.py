418d8fc2a24ca7134e39b74a7a490d85844411f5
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-02-04 12:30:29 -0600

    connected to sheety api and tequila api

--- data_manager.py
+++ data_manager.py
@@ -1,11 +1,33 @@
 import requests
 from dotenv import load_dotenv
 import os
+from pprint import pprint
 
 load_dotenv()
+
+SHEETY_PRICES_ENDPOINT = os.getenv('SHEETY_ENDPOINT')
+HEADERS = {
+    "Authorization": f"Bearer {os.getenv('BEARER_TOKEN')}"
+}
+
 
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
     def __init__(self):
-        self.endpoint = os.getenv('SHEETY_ENDPOINT')
-        
+        self.destination_data = {}
+
+    def get_request(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT, headers=HEADERS)
+        data = response.json()
+        self.destination_data = data['prices']
+        return self.destination_data
+
+    def put_request(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    'iataCode': city['iataCode']
+                }
+            }
+            response = requests.put(url=f'{SHEETY_PRICES_ENDPOINT}/{city["id"]}', headers=HEADERS, json=new_data)
+
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from data_manager import DataManager
+from flight_search import FlightSearch
 
-data = DataManager()
+data_manager = DataManager()
+sheet_data = data_manager.get_request()
+
+if sheet_data[0]['iataCode'] == '':
+  from flight_search import FlightSearch
+  flight_search = FlightSearch()
+  for row in sheet_data:
+    row['iataCode'] = flight_search.get_destination_code(row['city'])
+  data_manager.destination_data = sheet_data
+  data_manager.put_request()
+
+
+
+
