1ef91810ff846d6f5eeefaf7be2c32bbc2c19e58
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-02-05 12:30:29 -0600

    receiving city codes from tequila API and including them into google sheet with sheety API

--- flight_search.py
+++ flight_search.py
@@ -1,5 +1,24 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+ENDPOINT = os.getenv('TEQUILA_ENDPOINT')
+LOCATIONS_API = os.getenv('LOCATIONS_API')
+
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
     def get_destination_code(self, city_name):
-        code = "TESTING"
+        config = {
+            'term': city_name,
+            'location_types': 'city'
+        }
+        headers = {
+            'apikey': LOCATIONS_API
+        }
+        response = requests.get(url=f"{ENDPOINT}/locations/query", params=config, headers=headers)
+        data = response.json()
+        code = data['locations'][0]['code']
         return code
